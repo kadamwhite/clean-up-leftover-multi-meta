cba94ad8c7a31a168916e1935e995428b76545a2
K. Adam White <kadamwhite@users.noreply.github.com>
2022-09-25 14:35:55 -0400

    Initial commit: PHPCS and initial PHP file

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Clean Up Leftover Multi-Meta
+ * Plugin Version: 0.1.0
+ * Plugin Author: K. Adam White
+ */
+
+namespace CleanUPLeftoverMultiMeta;
+
+/**
+ * Connect namespace functions to actions & hooks.
+ */
+function boostrap() : void {
+    /* TK */
+}
+
+boostrap();
