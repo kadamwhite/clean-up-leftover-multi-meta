736f4c350147746af3dbc8be0e0af28552239fba
K. Adam White <kadamwhite@users.noreply.github.com>
2022-09-26 16:18:11 -0400

    Better spacing

--- inc/cli/class-clean-up-leftover-multi-meta.php
+++ inc/cli/class-clean-up-leftover-multi-meta.php
@@ -67,6 +67,7 @@ class Clean_Up_Leftover_Multi_Meta extends WP_CLI_Command {
 					if ( ( $meta_registration['show_in_rest'] ?? true ) === false ) {
 						continue;
 					}
+
 					$extant_meta = $wpdb->get_results(
 						$wpdb->prepare(
 							"select meta_id,meta_value,meta_key from {$wpdb->postmeta} where meta_key=%s and post_id=%d",
@@ -74,29 +75,35 @@ class Clean_Up_Leftover_Multi_Meta extends WP_CLI_Command {
 							$post->ID
 						)
 					);
+
 					if ( count( $extant_meta ) <= 1 ) {
 						continue;
 					}
+
 					if ( $verbose ) {
 						WP_CLI::log( "Processing $post_type id {$post->ID} $meta_key" );
 
 						// phpcs:ignore
 						Utils\format_items( 'table', $extant_meta, 'meta_id,meta_value,meta_key' );
 					}
+
 					foreach ( array_slice( $extant_meta, 1 ) as $idx => $meta ) {
 						if ( $verbose ) {
 							WP_CLI::log( sprintf( 'Discarding row with ID %d as a duplicate', $meta->meta_id ) );
 						}
+
 						if ( $dry_run ) {
 							WP_CLI::log( "Would delete {$meta_key} meta {$meta->meta_id} (Value '{$meta->meta_value}') for {$post_type} {$post->ID}" );
 						} else {
 							WP_CLI::log( "Deleting {$meta_key} meta {$meta->meta_id} (Value '{$meta->meta_value}') for {$post_type} {$post->ID}" );
+
 							$rows_affected = $wpdb->delete(
 								$wpdb->postmeta,
 								[ 'meta_id' => $meta->meta_id ]
 							);
+
 							if ( 0 < $rows_affected ) {
-								WP_CLI::success( "Deleted {$meta_key} meta {$meta->meta_id} (same as {$meta_key} meta {$extant_meta[0]->meta_id}) for {$post_type} {$post->ID}" );
+								WP_CLI::success( "Deleted {$meta_key} meta {$meta->meta_id} (superseded by {$meta_key} meta {$extant_meta[0]->meta_id}) for {$post_type} {$post->ID}" );
 							} elseif ( 0 === $rows_affected ) {
 								WP_CLI::warning( "No rows affected when trying to delete {$meta->meta_id} for {$post_type} {$post->ID}" );
 							} elseif ( false === $rows_affected ) {
